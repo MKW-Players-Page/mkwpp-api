99dafe30ed8af1f8c4a7db0c3b4edef3a64aa713
Nara Hiero <narahiero@gmail.com>
2025-04-29 14:33:23 -0400

    Fix player stats generation task when no group is provided (#34)

--- timetrials/tasks.py
+++ timetrials/tasks.py
@@ -4,6 +4,9 @@ from timetrials.models.stats import player_stats
 
 
 @shared_task
-def generate_player_stats(group_id):
-    group = player_stats.PlayerStatsGroup.objects.get(pk=group_id)
+def generate_player_stats(group_id=None):
+    if group_id is None:
+        group = None
+    else:
+        group = player_stats.PlayerStatsGroup.objects.get(pk=group_id)
     player_stats.generate_all_player_stats(group=group)
