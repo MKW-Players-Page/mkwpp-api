c29af1be5b4a9a56b24ae1e939cae4a85a49f73d
Nara Hiero <narahiero@gmail.com>
2025-05-02 13:52:44 -0400

    Fix player stats being generated twice (#35)

--- timetrials/tasks.py
+++ timetrials/tasks.py
@@ -6,7 +6,8 @@ from timetrials.models.stats import player_stats
 @shared_task
 def generate_player_stats(group_id=None):
     if group_id is None:
-        group = None
+        # This will automatically queue this task again with the ID of the created group
+        group = player_stats.PlayerStatsGroup.objects.create()
     else:
         group = player_stats.PlayerStatsGroup.objects.get(pk=group_id)
-    player_stats.generate_all_player_stats(group=group)
+        player_stats.generate_all_player_stats(group=group)
